7083c3bea91617c9b526465aef3e53c2fc597350
kumashun8 <tiwo3wa4@gmail.com>
2020-05-04 12:34:38 +0900

    Make home_controller

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,9 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { CircularProgress } from '@material-ui/core'
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <CircularProgress />
         Greeting: {this.props.greeting}
       </React.Fragment>
     );
